e7b710eae2c6ae1d80009b767fb7508b5ae4ab85
Richard Todd <waywardcode@users.noreply.github.com>
2016-01-13 13:38:15 -0600

    now spritz-hash accepts directory arguments, and hashes every file inside

--- cmd/spritz-hash/spritz-hash.go
+++ cmd/spritz-hash/spritz-hash.go
@@ -3,10 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/waywardcode/crypto/spritz"
 	"io"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"sync"
+
+	"github.com/waywardcode/crypto/spritz"
 )
 
 var bitSize = flag.Int("size", 256, "size of the hash in bits")
@@ -28,25 +31,56 @@ func runHash(fname string) {
 
 }
 
-func main() {
-	flag.Parse()
-	flist := flag.Args()
+// runFiles creates a goroutine to hash a file, or recurses on
+// directories.
+func runFiles(fi os.FileInfo,
+	path string,
+	limiter chan struct{},
+	wg *sync.WaitGroup) {
+	if fi.IsDir() {
 
-	// work on up to 8 files at a time
-	limiter := make(chan struct{}, 8)
+		allfiles, err := ioutil.ReadDir(path)
+		if err != nil {
+			fmt.Printf("%s: unable to read directory! %s\n",
+				path, err.Error())
+			return
+		}
 
-	// we will wait until all the goroutines are done...
-	var wg sync.WaitGroup
-	wg.Add(len(flist))
+		for _, fi := range allfiles {
+			newdir := filepath.Join(path, fi.Name())
+			runFiles(fi, newdir, limiter, wg)
+		}
 
-	for _, fname := range flist {
+	} else {
+
+		wg.Add(1)
 		go func(fname string) {
 			defer wg.Done()
 
 			limiter <- struct{}{} // take a slot
 			runHash(fname)
 			<-limiter // release the slot
-		}(fname)
+
+		}(path)
+
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	// work on up to 8 files at a time
+	limiter := make(chan struct{}, 8)
+
+	// we will wait until all the goroutines are done...
+	var wg sync.WaitGroup
+	for _, fname := range flag.Args() {
+		fi, err := os.Stat(fname)
+		if err == nil {
+			runFiles(fi, fname, limiter, &wg)
+		} else {
+			fmt.Printf("%s: unable to stat! %s\n", fname, err.Error())
+		}
 	}
 
 	wg.Wait() // sit here until everyting is reported
